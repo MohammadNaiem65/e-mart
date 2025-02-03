1130dc4557f57025475208d400b2c985dc239665
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-02-03 16:40:01 +0600

    add: created animated Categories component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Banner from './components/Home/Banner/Banner';
+import Categories from './components/Home/Categories/Categories';
 
 export default function Main() {
     return (
         <main className='bg-white font-inter'>
             <Banner />
+
+            <Categories />
         </main>
     );
 }
