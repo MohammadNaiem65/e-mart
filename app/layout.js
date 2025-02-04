63dba4fc8caa5ccd0911fec3d4c38401f778ce08
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-02-04 15:21:40 +0600

    update: replaced slick-carousel css file from page.js to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './globals.css';
 
 import Navbar from './components/shared/Navbar/Navbar';
--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import Banner from './components/Home/Banner/Banner';
 import Categories from './components/Home/Categories/Categories';
 import Coupons from './components/Home/Coupons/Coupons';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 export default function Main() {
     return (
