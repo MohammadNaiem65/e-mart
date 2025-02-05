a089750d17019c45e56dd547e4467f39d1b49b4d
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-02-05 11:04:42 +0600

    fix: added key to svg and fixed typo

--- app/components/shared/RatingStars/RatingStars.jsx
+++ app/components/shared/RatingStars/RatingStars.jsx
@@ -11,6 +11,7 @@ export default function RatingStars({
         <div className={className}>
             {Array.from({ length }).map((_, index) => (
                 <svg
+                    key={index}
                     width={width}
                     height={height}
                     viewBox='0 0 13 12'
@@ -18,8 +19,8 @@ export default function RatingStars({
                     fill={index + 1 <= rating ? activeColor : muteColor}
                 >
                     <path
-                        fill-rule='evenodd'
-                        clip-rule='evenodd'
+                        fillRule='evenodd'
+                        clipRule='evenodd'
                         d='M6.15065 8.90644L2.42288 11.7541L3.97925 7.32883L0.118996 4.6635L4.80865 4.7762L6.15065 0.28125L7.49264 4.7762L12.1823 4.6635L8.32204 7.32883L9.87841 11.7541L6.15065 8.90644Z'
                     />
                 </svg>
