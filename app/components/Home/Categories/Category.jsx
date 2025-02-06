dfd2d046a5157aa48a97e4f3152d8d9e01f9eaef
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-02-06 09:21:07 +0600

    fix: fixed image border radius

--- app/components/Home/Categories/Category.jsx
+++ app/components/Home/Categories/Category.jsx
@@ -7,7 +7,11 @@ export default function Category({ category }) {
                 style={{ backgroundImage: 'url(./category-bg.svg)' }}
                 className='size-[6.875rem] p-1 border-[3px] border-[#E1DCD0] bg-no-repeat bg-center flex items-center justify-center rounded-full'
             >
-                <img src='./category.png' alt={name} className='size-24' />
+                <img
+                    src='./category.png'
+                    alt={name}
+                    className='size-24 rounded-full'
+                />
             </div>
 
             <span className='font-bold text-sm truncate'>{name}</span>
