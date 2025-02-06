2981188cefc83ad52d8718697c5189f4d3f0bbf8
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-02-06 09:35:12 +0600

    add: created ThirdBanner component

---
+++ app/components/Home/ThirdBanner/ThirdBanner.jsx
@@ -0,0 +1,16 @@
+export default function ThirdBanner() {
+    return (
+        <section className="mt-11 flex justify-between items-center">
+            <img
+                src='/banner-3-left.png'
+                alt='banner image'
+                className='w-[36.75rem] h-[15.625rem] rounded-[0.625rem]'
+            />
+            <img
+                src='./banner-3-right.png'
+                alt='banner image'
+                className='w-[36.75rem] h-[15.625rem] rounded-[0.625rem]'
+            />
+        </section>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Coupons from './components/Home/Coupons/Coupons';
 import FirstCategoryProducts from './components/Home/FirstCategoryProducts/FirstCategoryProducts';
 import FlashSale from './components/Home/FlashSale/FlashSale';
 import SecondBanner from './components/Home/SecondBanner/SecondBanner';
+import ThirdBanner from './components/Home/ThirdBanner/ThirdBanner';
 
 export default function Main() {
     return (
@@ -19,6 +20,8 @@ export default function Main() {
             <SecondBanner />
 
             <FirstCategoryProducts />
+
+            <ThirdBanner />
         </main>
     );
 }
