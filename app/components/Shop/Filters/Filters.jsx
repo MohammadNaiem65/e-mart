8b7fa2b03a3b3111f8e2276a63e79ec8b6295058
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-02-06 17:13:30 +0600

    add: created /shop page layout

---
+++ app/components/Shop/Filters/Filters.jsx
@@ -0,0 +1,9 @@
+import Pagination from '../../shared/Pagination/Pagination';
+
+export default function Filters() {
+    return (
+        <section className='col-span-3'>
+            <h2 className='pb-1 border-b border-stroke text-[1.375rem] font-semibold'>Filter Product</h2>
+        </section>
+    );
+}
---
+++ app/components/Shop/Products/Products.jsx
@@ -0,0 +1,5 @@
+export default function Products() {
+    return <section className='col-span-9'>
+        <h2 className='pb-1 border-b border-stroke text-[1.375rem] font-semibold'>All Products</h2>
+    </section>;
+}
