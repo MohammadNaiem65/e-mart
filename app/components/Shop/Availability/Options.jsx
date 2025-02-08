1ec825c0fc4cc9f71908cfb9759bec1435e8a35a
dev7-ngenit <dev7.ngenit@gmail.com>
2025-02-08 17:27:46 +0600

    update: updated the style

--- app/components/Shop/Availability/Options.jsx
+++ app/components/Shop/Availability/Options.jsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { IoMdCheckmark } from 'react-icons/io';
-// import CategoryInput from './CategoryInput';
 
 export default function Options() {
     const [options, setOptions] = useState([
@@ -11,28 +10,21 @@ export default function Options() {
     ]);
 
     return (
-        <section className='mt-3'>
-            {/* <CategoryInput /> */}
+        <section className='flex flex-col gap-y-2 mt-4 overflow-y-auto'>
+            {options?.map((category, index) => (
+                <div key={index} className='flex items-center gap-x-3 relative'>
+                    <input
+                        id={index}
+                        type='checkbox'
+                        name={category?.name}
+                        value={category?.name}
+                        className='size-6 p-2 appearance-none peer border-2 rounded-lg checked:bg-primary checked:border-primary'
+                    />
+                    <IoMdCheckmark className='text-white text-lg hidden absolute left-3 top-1/2 -translate-x-1/2 -translate-y-1/2 peer-checked:block' />
 
-            <div className='flex flex-col gap-y-2 mt-4 overflow-y-auto'>
-                {options?.map((category, index) => (
-                    <div
-                        key={index}
-                        className='flex items-center gap-x-3 relative'
-                    >
-                        <input
-                            id={index}
-                            type='checkbox'
-                            name={category?.name}
-                            value={category?.name}
-                            className='size-6 p-2 appearance-none peer border-2 rounded-lg checked:bg-primary checked:border-primary'
-                        />
-                        <IoMdCheckmark className='text-white text-lg hidden absolute left-3 top-1/2 -translate-x-1/2 -translate-y-1/2 peer-checked:block' />
-
-                        <label htmlFor={index}>{category?.name}</label>
-                    </div>
-                ))}
-            </div>
+                    <label htmlFor={index}>{category?.name}</label>
+                </div>
+            ))}
         </section>
     );
 }
