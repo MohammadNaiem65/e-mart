2bf75f15cfb085a6835adf2f39870ea9e2f17c35
dev7-ngenit <dev7.ngenit@gmail.com>
2025-02-12 09:52:19 +0600

    update: updated the title size

--- app/components/Shop/product/RelatedProducts/RelatedProducts.jsx
+++ app/components/Shop/product/RelatedProducts/RelatedProducts.jsx
@@ -3,16 +3,14 @@ import HorizontalCard from '../../shared/HorizontalCard';
 export default function RelatedProducts() {
     return (
         <section className='col-span-4'>
-            <h3 className='pb-2 border-b border-stroke text-2xl font-semibold'>
-                More Related Product{' '}
+            <h3 className='pb-2 border-b border-stroke text-xl font-semibold text-dark'>
+                More Related Products
             </h3>
 
             <section>
-                {
-                    Array.from({ length: 8 }).map((_, index) => (
-                        <HorizontalCard key={index} />
-                    ))
-                }
+                {Array.from({ length: 8 }).map((_, index) => (
+                    <HorizontalCard key={index} />
+                ))}
             </section>
         </section>
     );
